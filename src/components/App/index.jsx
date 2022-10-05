f069bac16da4b19f36f10afbede833e04a307618
Michael <m.klepcha42@mail.ru>
2022-10-05 12:55:31 +0300

    created a footer

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './style.scss'
 import StoreHeader from '../StoreHeader';
 import StorageContent from '../StoreContent';
+import StoreFooter from '../StoreFooter';
 
 function App() {
   return (
     <div className="main-container">
       <StoreHeader />
       <StorageContent />
+      <StoreFooter />
     </div>
   );
 }
---
+++ src/components/StoreFooter/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.scss'
+
+export default function StoreFooter() {
+    return (
+        <footer className='footer-container'>
+            <div className='footer-main-logo'>
+                <img src='./img/logos/header-logo.png' alt='footer-logo'/>
+            </div>
+            <div className='footer-links'>
+                <ul className='footer-links-list'>
+                    <li className='footer-links-list-item'>Избранное</li>
+                    <li className='footer-links-list-item'>Корзина</li>
+                    <li className='footer-links-list-item'>Контакты</li>
+                </ul>
+            </div>
+            <div className='footer-service-conditions'>
+                <div><p>Условия сервиса</p></div>
+                <div className='footer-local-logo'>
+                    <div>
+                        <img src='./img/logos/earth-logo.png' alt='earth-logo'/>
+                    </div>
+                    <span className='rus-local'>Рус</span>
+                    <span>Eng</span>
+                </div>
+            </div>
+            <div className='footer-social-logos'>
+                <img src='./img/logos/social-logos.png' alt='social-logos'/>
+            </div>
+        </footer>
+    )
+}
