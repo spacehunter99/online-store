2f45ab5ab48879ddabf032dd62056cad833920fb
Michael <m.klepcha42@mail.ru>
2022-10-10 15:58:39 +0300

    added additional link in header

--- src/components/StoreHeader/index.tsx
+++ src/components/StoreHeader/index.tsx
@@ -10,16 +10,20 @@ interface StoreHeaderProps {
 export default function StoreHeader({chosenGoods}: StoreHeaderProps) {
     return (
         <nav className='navigation'>
-            <div className='headerLogo'>
-                <img src="./img/logos/header-logo.png" alt='main_logo'/>
-            </div>
+            <Link to="/">
+                <div className='headerLogo'>
+                    <img src="./img/logos/header-logo.png" alt='main_logo'/>
+                </div>
+            </Link>
             <div className='likes-and-cart-logos'>
                 <div className='heart-logo'>
                     <div className='heart-count-container'><span className='heart-counter'>2</span></div>
                 </div>
-                <Link to="/cart"><div className='cartLogo'>
-                    <div className='cart-count-container'><span className='cart-counter'>{chosenGoods.length - 1}</span></div>
-                </div></Link>
+                <Link to="/cart">
+                    <div className='cartLogo'>
+                        <div className='cart-count-container'><span className='cart-counter'>{chosenGoods.length - 1}</span></div>
+                    </div>
+                </Link>
             </div>
         </nav>
     )
